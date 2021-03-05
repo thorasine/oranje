e4a9613c6ceec9973a1f72b37f50e6860b29e1fc
thorasine <zajakrisz@gmail.com>
2021-03-05 18:37:36 +0100

    Initial commit

---
+++ src/main/java/thorasine/oranje/OranjeApplication.java
@@ -0,0 +1,14 @@
+package thorasine.oranje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OranjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OranjeApplication.class, args);
+	}
+
+}
